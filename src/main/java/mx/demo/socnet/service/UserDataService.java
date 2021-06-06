537345f22bacf7af2fd8782a0155182b368d05df
Mladen Nikolic <mladen.nikolic.mex@gmail.com>
2021-06-06 13:24:16 +0200

    Added copyright formatting new empty service
    Signed-off-by: Mladen Nikolic <mladen.nikolic.mex@gmail.com>

--- src/main/java/mx/demo/socnet/service/UserDataService.java
+++ src/main/java/mx/demo/socnet/service/UserDataService.java
@@ -14,40 +14,19 @@
  * limitations under the License.
  */
 
-package mx.demo.socnet.data.entity;
+package mx.demo.socnet.service;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Mladen Nikolic <mladen.nikolic.mex@gmail.com>
  * https://www.linkedin.com/in/mladen-nikolic-mex/
- * @created 04.06.2021
+ * @created 06.06.2021
  * @project socnet
  */
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table
-@ToString
-public class UserData {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private Boolean admin;
-    private String gender;
-    private String country;
-    private String password;
-    @OneToMany(mappedBy = "user")
-    private List<UserPost> posts;
+@Slf4j
+@Service
+public class UserDataService {
 }
